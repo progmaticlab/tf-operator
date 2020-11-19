b9db0fce53e6b23f253778c9d6a22f42e5eb1791
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-11-19 16:33:43 +0000

    contrail to tf

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-operator-sdk build localhost:5000/contrail-operator:latest
-docker push localhost:5000/contrail-operator:latest
+operator-sdk build localhost:5000/tf-operator:latest
+docker push localhost:5000/tf-operator:latest
 
 # build CRDS container
 
 docker build --tag localhost:5000/crdsloader:latest deploy/crds
-localhost:5000/crdsloader:latest
+docker push localhost:5000/crdsloader:latest
