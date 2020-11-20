b33df05f559531d59f25a7e73092efe3468a6bb4
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-11-20 07:56:54 +0000

    fix bash test

--- scripts/run_operator.sh
+++ scripts/run_operator.sh
@@ -2,7 +2,7 @@
 
 WORKSPACE=${WORKSPACE:-$HOME/tf-operator}
 kubectl apply -k ${WORKSPACE}/deploy/kustomize/operator/latest/
-while [ ! $(kubectl wait crds --for=condition=Established --timeout=2m managers.contrail.juniper.net) ]
+while [[ ! $(kubectl wait crds --for=condition=Established --timeout=2m managers.contrail.juniper.net) ]]
 do
   sleep 2s
 done
