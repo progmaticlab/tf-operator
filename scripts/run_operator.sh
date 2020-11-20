72d789997a8c09c57a43f439247717a64380e882
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-11-20 07:53:06 +0000

    Wait for crds

--- scripts/run_operator.sh
+++ scripts/run_operator.sh
@@ -2,4 +2,9 @@
 
 WORKSPACE=${WORKSPACE:-$HOME/tf-operator}
 kubectl apply -k ${WORKSPACE}/deploy/kustomize/operator/latest/
+while [ ! $(kubectl wait crds --for=condition=Established --timeout=2m managers.contrail.juniper.net) ]
+do
+  sleep 2s
+done
+
 kubectl apply -k ${WORKSPACE}/deploy/kustomize/contrail/1node/latest/
