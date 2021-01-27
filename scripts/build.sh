7591b6e836991428e5343dda2c06891e2fe0d70b
tikitavi <rtikitavi@gmail.com>
2021-01-27 13:46:13 +0300

    add opportunity for parametrizing operator containers
    Signed-off-by: tikitavi <rtikitavi@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,13 @@
 type go >/dev/null 2>&1 || {
   export PATH=$PATH:/usr/local/go/bin
 }
+CONTAINER_REGISTRY=${CONTAINER_REGISTRY:-"localhost:5000"}
+CONTRAIL_CONTAINER_TAG=${CONTRAIL_CONTAINER_TAG:-"latest"}
 
 export  CGO_ENABLED=1
-operator-sdk build  localhost:5000/tf-operator:latest
-sudo docker push localhost:5000/tf-operator:latest
+operator-sdk build  ${CONTAINER_REGISTRY}/tf-operator:${CONTRAIL_CONTAINER_TAG}
+sudo docker push ${CONTAINER_REGISTRY}/tf-operator:${CONTRAIL_CONTAINER_TAG}
 
 # build CRDS container
-sudo docker build --tag localhost:5000/tf-crdsloader:latest deploy/crds
-sudo docker push localhost:5000/tf-crdsloader:latest
+sudo docker build --tag ${CONTAINER_REGISTRY}/tf-crdsloader:${CONTRAIL_CONTAINER_TAG} deploy/crds
+sudo docker push ${CONTAINER_REGISTRY}/tf-crdsloader:${CONTRAIL_CONTAINER_TAG}
