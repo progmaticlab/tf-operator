ca38942a485b08adc3822eed99e747aeb2930028
tikitavi <rtikitavi@gmail.com>
2021-01-27 13:46:13 +0300

    moved registry for tf-operator container into parameter
    Signed-off-by: tikitavi <rtikitavi@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
+CONTAINER_REGISTRY=${CONTAINER_REGISTRY:-"localhost:5000"}
+
 export  CGO_ENABLED=1
-operator-sdk build  localhost:5000/tf-operator:latest
-sudo docker push localhost:5000/tf-operator:latest
+operator-sdk build  ${CONTAINER_REGISTRY}/tf-operator:latest
+sudo docker push ${CONTAINER_REGISTRY}/tf-operator:latest
 
 # build CRDS container
-sudo docker build --tag localhost:5000/tf-crdsloader:latest deploy/crds
-sudo docker push localhost:5000/tf-crdsloader:latest
+sudo docker build --tag ${CONTAINER_REGISTRY}/tf-crdsloader:latest deploy/crds
+sudo docker push ${CONTAINER_REGISTRY}/tf-crdsloader:latest
