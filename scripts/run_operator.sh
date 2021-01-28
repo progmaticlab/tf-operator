9fe7aa1d8ee4c0118a38e5eff2424e330027b1f8
tikitavi <rtikitavi@gmail.com>
2021-01-28 13:55:37 +0300

    move registry and tags of containers to parameters
    Signed-off-by: tikitavi <rtikitavi@gmail.com>

--- scripts/run_operator.sh
+++ scripts/run_operator.sh
@@ -1,20 +1,26 @@
 #!/bin/bash -e
-
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
 WORKSPACE=${WORKSPACE:-$HOME/tf-operator}
 
-# multinode setup with registry on one node
+export CONTAINER_REGISTRY=${CONTAINER_REGISTRY:-"tungstenfabric"}
+export CONTRAIL_CONTAINER_TAG=${CONTRAIL_CONTAINER_TAG:-"latest"}
+
 count=$(echo "$CONTROLLER_NODES $AGENT_NODES" | tr " " "\n" | sort -u |  tr "\n" " " | awk -F ' ' '{print NF}' )
-if [[ $count > 1 ]] ; then
+if [[ $count > 1 ]] && [[ $CONTAINER_REGISTRY == localhost* ]] ; then
+  # multinode setup with registry on one node
   registry_ip=$(hostname -i)
-  sed -i "s/localhost/$registry_ip/g" ${WORKSPACE}/deploy/kustomize/operator/latest/*
-  sed -i "s/localhost/$registry_ip/g" ${WORKSPACE}/deploy/kustomize/base/operator/*
-  sed -i "s/localhost/$registry_ip/g" ${WORKSPACE}/deploy/kustomize/contrail/1node/latest/*
+  export CONTAINER_REGISTRY=$(echo $CONTAINER_REGISTRY | sed "s/loclahost/$registry_ip/g")
 fi
 
-kubectl apply -k ${WORKSPACE}/deploy/kustomize/operator/latest/
+OPERATOR_KUSTOMIZATION_FILE="$my_dir/../deploy/kustomize/operator/latest/kustomization.yaml"
+"$my_dir/jinja2_render.py" < ${OPERATOR_KUSTOMIZATION_FILE}.j2 > ${OPERATOR_KUSTOMIZATION_FILE}
+kubectl apply -k $my_dir/../deploy/kustomize/operator/latest/
 while [[ ! $(kubectl wait crds --for=condition=Established --timeout=2m managers.contrail.juniper.net) ]]
 do
   sleep 2s
 done
 
-kubectl apply -k ${WORKSPACE}/deploy/kustomize/contrail/1node/latest/
+CONTRAIL_KUSTOMIZATION_FILE="$my_dir/../deploy/kustomize/contrail/1node/latest/kustomization.yaml"
+"$my_dir/jinja2_render.py" < ${CONTRAIL_KUSTOMIZATION_FILE}.j2 > ${CONTRAIL_KUSTOMIZATION_FILE}
+kubectl apply -k $my_dir/../deploy/kustomize/contrail/1node/latest/
