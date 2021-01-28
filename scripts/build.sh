4a5647fbe422f2e3f2a0815e7bf9d9e27ed7fde5
tikitavi <rtikitavi@gmail.com>
2021-01-28 13:55:37 +0300

    move registry and tags of containers to parameters
    Signed-off-by: tikitavi <rtikitavi@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 CONTAINER_REGISTRY=${CONTAINER_REGISTRY:-"localhost:5000"}
+CONTRAIL_CONTAINER_TAG=${CONTRAIL_CONTAINER_TAG:-"latest"}
 
 export  CGO_ENABLED=1
-operator-sdk build  ${CONTAINER_REGISTRY}/tf-operator:latest
-sudo docker push ${CONTAINER_REGISTRY}/tf-operator:latest
+operator-sdk build  ${CONTAINER_REGISTRY}/tf-operator:${CONTRAIL_CONTAINER_TAG}
+sudo docker push ${CONTAINER_REGISTRY}/tf-operator:${CONTRAIL_CONTAINER_TAG}
 
 # build CRDS container
-sudo docker build --tag ${CONTAINER_REGISTRY}/tf-crdsloader:latest deploy/crds
-sudo docker push ${CONTAINER_REGISTRY}/tf-crdsloader:latest
+sudo docker build --tag ${CONTAINER_REGISTRY}/tf-crdsloader:${CONTRAIL_CONTAINER_TAG} deploy/crds
+sudo docker push ${CONTAINER_REGISTRY}/tf-crdsloader:${CONTRAIL_CONTAINER_TAG}
