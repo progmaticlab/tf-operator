4f3b211a81bfa2cca396fe64a6019d07ee66fdb5
alexey-mr <alexey.morlang@gmail.com>
2021-01-29 12:29:47 +0300

    use tf niglhy byt default in run

--- scripts/run_operator.sh
+++ scripts/run_operator.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -e
 
 WORKSPACE=${WORKSPACE:-$HOME/tf-operator}
-kubectl apply -k ${WORKSPACE}/deploy/kustomize/operator/latest/
+
+OPERATOR_VERSION=${OPERATOR_VERSION:-'latest'}
+CONTRAIL_VERSION=${CONTRAIL_VERSION:-'tf-ci-nightly'}
+CONTRAIL_HA=${CONTRAIL_HA:-'1node'}
+
+kubectl apply -k ${WORKSPACE}/deploy/kustomize/operator/$OPERATOR_VERSION/
 while [[ ! $(kubectl wait crds --for=condition=Established --timeout=2m managers.contrail.juniper.net) ]]
 do
   sleep 2s
 done
 
-kubectl apply -k ${WORKSPACE}/deploy/kustomize/contrail/1node/latest/
+kubectl apply -k ${WORKSPACE}/deploy/kustomize/contrail/$CONTRAIL_HA/$CONTRAIL_VERSION/
