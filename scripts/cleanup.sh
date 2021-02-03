c519a3e466a0808ad164459b6ab68d8dd9ebc147
alexey-mr <alexey.morlang@gmail.com>
2021-02-03 17:48:33 +0300

    fix

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -x
 
-kubectl delete -k deploy/kustomize/contrail/1node/latest/
-kubectl delete -k deploy/kustomize/operator/latest/
-kubectl delete -f deploy/crds/
+WORKSPACE=${WORKSPACE:-$HOME}
+
+cd $WORKSPACE
+
+kubectl delete -k ./tf-operator/deploy/kustomize/contrail/1node/latest/
+kubectl delete -k ./tf-operator/deploy/kustomize/operator/latest/
+kubectl delete -f ./tf-operator/deploy/crds/
 
 # TODO: implement for HA setup
 kubectl delete pv  cassandra1-pv-0 zookeeper1-pv-0
