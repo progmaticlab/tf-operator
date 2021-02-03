0b4edac2adc0657be133fd629cc0ad6b4da96fa4
alexey-mr <alexey.morlang@gmail.com>
2021-02-03 17:41:21 +0300

    fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,11 +4,15 @@ type go >/dev/null 2>&1 || {
   export PATH=$PATH:/usr/local/go/bin
 }
 
+export WORKSPACE=${WORKSPACE:-$HOME}
 export CONTRAIL_REPOSITORY=${CONTRAIL_REPOSITORY:-"localhost:5000"}
 export CONTRAIL_CONTAINER_TAG=${CONTRAIL_CONTAINER_TAG:-"latest"}
 export CGO_ENABLED=1
 
 target=${CONTRAIL_REPOSITORY}/${CONTRAIL_CONTAINER_TAG}
+pushd ${WORKSPACE}/tf-operator
 
 operator-sdk build $target
 docker push $target
+
+popd
