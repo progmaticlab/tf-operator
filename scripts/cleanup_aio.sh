b88fb9dd48de74e17b3fce37890d32ed1abad0b8
alexey-mr <alexey.morlang@gmail.com>
2021-02-03 13:54:31 +0300

    changes in cleanup

--- scripts/cleanup_aio.sh
+++ scripts/cleanup_aio.sh
@@ -4,4 +4,10 @@ kubectl delete -k deploy/kustomize/contrail/1node/latest/
 kubectl delete -k deploy/kustomize/operator/latest/
 kubectl delete -f deploy/crds/
 kubectl delete pv  cassandra1-pv-0 zookeeper1-pv-0
-sudo rm -rf /mnt/cassandra /mnt/zookeeper /var/lib/contrail /var/log/contrail
+sudo rm -rf \
+  /mnt/cassandra \
+  /mnt/zookeeper \
+  /var/lib/contrail \
+  /var/log/contrail \
+  /var/crashes/contrail \
+  /etc/cni/net.d/10-tf-cni.conf
