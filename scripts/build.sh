aabbb83f6d894225da4260f7d35189824cec6766
alexey-mr <alexey.morlang@gmail.com>
2021-02-03 17:44:23 +0300

    fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,7 @@ export CONTRAIL_REPOSITORY=${CONTRAIL_REPOSITORY:-"localhost:5000"}
 export CONTRAIL_CONTAINER_TAG=${CONTRAIL_CONTAINER_TAG:-"latest"}
 export CGO_ENABLED=1
 
-target=${CONTRAIL_REPOSITORY}/${CONTRAIL_CONTAINER_TAG}
+target=${CONTRAIL_REPOSITORY}/tf-operator:${CONTRAIL_CONTAINER_TAG}
 pushd ${WORKSPACE}/tf-operator
 
 operator-sdk build $target
